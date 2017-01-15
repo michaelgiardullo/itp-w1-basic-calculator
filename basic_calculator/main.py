2dd857fe7d2243a8eb10500e80190371bb24ab77
Michael Giardullo <michaelgiardullo@gmail.com>
2017-01-15 18:32:46 +0000

    fully working solution

--- basic_calculator/main.py
+++ basic_calculator/main.py
@@ -1,5 +1,18 @@
 """This is the entry point of the program."""
 
 
-def basic_calculator(num1, num2, operation):
-    pass
+def basic_calculator(num1,num2,operation):
+    
+    if isinstance(num1,str) == True or isinstance(num2,str) == True:
+        return "Invalid Inputs"
+    
+    if operation == 'add':
+        return num1+num2
+    elif operation == 'subtract':
+        return num1-num2
+    elif operation == 'multiply':
+        return num1*num2
+    elif operation == 'divide':
+        return num1/num2
+    else:
+        return "Invalid operation"
